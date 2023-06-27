1239edce173bc90f00897e2ddd95c71f20b14ab5
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-06-26 23:32:25 -0400

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function App() {
+  const [ wordToGuess, setWordToGuess ] = useState('Test')
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
