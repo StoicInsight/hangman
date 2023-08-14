f4c068b15a4d315bb7e0f3f1adca2fc434a8407e
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-08-13 22:29:12 -0400

    added keyboard component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const HangmanWord = ({ word }) => {
+  
+
   return (
     <div style={{ display: 'flex', gap: '.25em', fontSize:'6rem', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'monospace' }}>
       {word.split('').map((letter, index) => (
-        <span style={{ borderBottom: '.1em solid black'}}>
+        <span style={{
+            visibility: guessedLetters.includes(letter)
+              ? 'visible'
+              : 'hidden',
+            borderBottom: '.1em solid black'}}>
           {letter}
         </span>
       ))}
